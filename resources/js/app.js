981374ace695510179a52c52ed50f2ed5410ba85
macaron226 <macabot0123@gmail.com>
2021-09-19 06:59:18 +0900

    add app-bridge-react

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,12 +2,22 @@ require('./bootstrap');
 
 import React from 'react';
 import ReactDOM from 'react-dom'
+import { Provider } from '@shopify/app-bridge-react';
+import enTranslations from '@shopify/polaris/locales/en.json';
+import { AppProvider } from '@shopify/polaris';
 
 const App = () => {
+  const config = {
+    apiKey: document.getElementById("apiKey").value,
+    host: document.getElementById("host").value,
+  };
+
   return (
-    <div>
-      React test!
-    </div>
+    <AppProvider i18n={ enTranslations}>
+      <Provider config={ config }>
+        React test!
+      </Provider>
+    </AppProvider>
   )
 };
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,9 @@
 @section('content')
     <!-- You are: (shop domain name) -->
 {{--    <p>You are: {{ $shopDomain ?? Auth::user()->name }}</p>--}}
+    <input type="hidden" id="apiKey" value="{{ config('shopify-app.api_key') }}">
+    <input type="hidden" id="host" value="{{Request::get('host')}}">
+
     <div id="app"></div>
     <script src="{{mix('js/app.js')}}"></script>
 
